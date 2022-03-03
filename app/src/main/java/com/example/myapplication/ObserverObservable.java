99f33b35c5f792a5f271faac833b92f43e2ff1c2
Ramesh M <62267259+ramdisk-ott@users.noreply.github.com>
2022-03-03 13:40:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ObserverObservable.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+
+public class ObserverObservable extends AppCompatActivity {
+    private String TAG = "MainActivity";
+    String greeting = "This is ramesh";
+    private Observable<String> observable;
+    private Observer<String> observer;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        observable = Observable.just(greeting);
+        textView = findViewById(R.id.title);
+        observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.d(TAG, "onNext: ");
+                textView.setText(s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        };
+        observable.subscribe(observer);
+    }
+}
